e40616536c36a7f60b8c66232df52349cc496e69
GKGuddu <gkgudd860@gmail.com>
2026-08-09 19:30:51 +0530

    Update footer and resume

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="w-full bg-gray-900 text-gray-400 py-4 flex justify-center items-center border-t border-gray-800">
             <p className="text-center text-sm md:text-base">
-                &copy; {new Date().getFullYear()} Guddu Kumar. All rights reserved.
+                2024 &copy;  Guddu Kumar. All rights reserved.
             </p>
         </footer>
     );
